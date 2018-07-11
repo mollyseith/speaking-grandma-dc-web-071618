4afdeb06fd40f7f6e5f73cae774cbbaecdbeb79d
Molly Seither <molly.seither@centre.edu>
2018-07-11 17:50:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
   if (phrase == "I LOVE YOU GRANDMA!")
       return "I LOVE YOU TOO PUMPKIN!"
-  elsif (phrase.upcase)
+  elsif (phrase.upcase == phrase)
       return "NO, NOT SINCE 1938!"
-  elsif (phrase.upcase)
+  elsif (phrase.upcase == phrase)
       return "NO, NOT SINCE 1938!"
-  else (phrase.downcase)
+  else (phrase.upcase != phrase)
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
